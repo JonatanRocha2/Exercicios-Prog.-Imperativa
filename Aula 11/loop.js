cbcbaf53fd6806299b507e0c12c97af66b154118
Jonatan Ivanhoe da Silva Rocha <jonatanivanhoe@gmail.com>
2022-05-28 12:04:26 -0300

    exercicios loop for

---
+++ Aula 11/loop.js
@@ -0,0 +1,30 @@
+/* Crie um loop for de 1 a 10, em que exiba no console apenas o valor das repetições de números ímpares. Dica: A ideia é que nas repetições ímpares de 0 ao 10 (1,3,5,7,9) sejam mostrados os números das repetições no console.
+ */
+
+for( let i=1; i<=10; i++) {
+    if( i%2==1 ){
+        console.log(i + ` é ímpar\n`);
+    }
+} 
+
+
+
+/* Crie um loop utilizando for que se repita 5 vezes. Dentro de cada repetição se deve mostrar no console a mensagem “Olá mundo”. */
+
+for(let i=0; i<=5; i++){
+    console.log(`Olá mundo!\n`);
+} 
+
+
+/* Crie uma tabuada utilizando o for. Dica: Para essa tarefa você irá precisar utilizar dois loops (for). */
+
+for(let i=1; i<=10; i++){
+    console.log(`\nTabuada do ${i}\n`);
+
+    for(let j=1; j<=10; j++){
+        console.log(`${i} x ${j} é igual a ${i*j}\n`);
+        console.log(`${i} + ${j} é igual a ${i+j}\n`);
+        console.log(`${i} - ${j} é igual a ${i-j}\n`);
+        console.log(`${i} / ${j} é igual a ${i/j}\n`);
+    }
+}
