2e8a31bbff3eb802b95b210e6a2fb27b9ca39906
Jonatan Ivanhoe da Silva Rocha <jonatanivanhoe@gmail.com>
2022-06-09 20:47:46 -0300

    exercicio mesa 18

--- Aula 13/app.js
+++ Aula 13/app.js
@@ -14,91 +14,103 @@ function Conta(numeroConta, tipoConta, saldo, titular){
 }
 
 //instancia de clientes
-let abigaelNatte = new Conta(
+let conta1 = new Conta(
     5486273622,
     'ContaCorrente',
     27771,
     'AbigaelNatte'
 );
 
-let ramonConnel = new Conta(
+let conta2 = new Conta(
     1183971869,
     'ContaPoupanca',
     8675,
     'RamonConnell'
 );
 
-let jarretLafuente = new Conta(
+let conta3 = new Conta(
     9582019689,
     'ContaPoupanca',
     27363,
     'JarretLafuente'
 );
 
-let anselArdley = new Conta(
+let conta4 = new Conta(
     1761924656,
     'ContaPoupanca',
     32415,
     'AnselArdley'
 );
 
-let jackiShurmer = new Conta(
+let conta5 = new Conta(
     7401971607,
     'ContaPoupanca',
     18789,
     'JackiShurmer'
 );
 
-let jobiMawtus = new Conta(
+let conta6 = new Conta(
     630841470,
     'ContaCorrente',
     28776,
     'JobiMawtus'
 );
 
-let thomasinLatour = new Conta(
+let conta7 = new Conta(
     4223508636,
     'ContaCorrente',
     2177,
     'ThomasinLatour'
 );
 
-let lonnieVerheijden = new Conta(
+let conta8 = new Conta(
     185979521,
     'ContaPoupanca',
     25994,
     'LonnieVerheijden'
 );
 
-let alonsoWannan = new Conta(
+let conta9 = new Conta(
     3151956165,
     'ContaCorrente',
     7601,
     'AlonsoWannan'
 );
 
-let benditeHuggett = new Conta(
+let conta10 = new Conta(
     2138105881,
     'ContaPoupanca',
     33196,
     'BenditeHuggett'
 );
 
-//console.log(thomasinLatour)
-
 //lista de clientes
-let listaContas = [abigaelNatte, ramonConnel, jackiShurmer, anselArdley, jackiShurmer, jobiMawtus, thomasinLatour, lonnieVerheijden, alonsoWannan, benditeHuggett];
-//console.log(listaContas);
+let listaContas = {
+    1: {conta1},
+    2: {conta2},
+    3: {conta3},
+    4: {conta4},
+    5: {conta5},
+    6: {conta6},
+    7: {conta7},
+    8: {conta8},
+    9: {conta9},
+    10: {conta10}
+}
+
 
 //obj banco recebe a lista de clientes
 const banco = {
     clientes: listaContas,
     consultarClientes(nome){
-        this.nome = listaContas[i];
+        this.nome = listaContas;
         return nome;
     },
-    deposito(valor){
-        Conta.saldo += valor;
+    deposito(titular, valor){
+        
+              
+
+
         console.log(`Depósito realizado, seu novo saldo é: "R$"${valor}`)
     },
     //saca se valor atual na conta for maior ou igual ao valor do solicitado
@@ -115,5 +127,8 @@ const banco = {
     }
 }
 
-//console.log(banco);
+
+
+
+
 
--- Conta.js
+++ Conta.js
@@ -1,79 +1,63 @@
 //funcao construtora conta
 
-function Conta(numeroConta, tipoConta, saldo, titular){
-    this.numeroConta = numeroConta;
-    this.tipoConta = tipoConta;
-    this.saldo = saldo;
-    this.titular = titular;    
+function Conta(numeroConta, tipoConta, saldo, titular) {
+  this.numeroConta = numeroConta;
+  this.tipoConta = tipoConta;
+  this.saldo = saldo;
+  this.titular = titular;
 }
 
 //instancia de clientes
 
-let abigaelNatte = new Conta(5486273622, 'ContaCorrente', 27771, 'AbigaelNatte');
+let abigaelNatte = new Conta(5486273622, "ContaCorrente", 27771, "AbigaelNatte");
 
-let ramonConnel = new Conta(1183971869, 'ContaPoupanca', 8675, 'RamonConnell');
+let ramonConnel = new Conta(1183971869, "ContaPoupanca", 8675, "RamonConnell");
 
-let jarretLafuente = new Conta(9582019689, 'ContaPoupanca', 27363, 'JarretLafuente');
+let jarretLafuente = new Conta(9582019689, "ContaPoupanca", 27363, "JarretLafuente");
 
-let anselArdley = new Conta(1761924656, 'ContaPoupanca', 32415, 'AnselArdley');
+let anselArdley = new Conta(1761924656, "ContaPoupanca", 32415, "AnselArdley");
 
-let jackiShurmer = new Conta(7401971607, 'ContaPoupanca', 18789, 'JackiShurmer');
+let jackiShurmer = new Conta(7401971607, "ContaPoupanca", 18789, "JackiShurmer");
 
-let jobiMawtus = new Conta(630841470, 'ContaCorrente', 28776, 'JobiMawtus');
+let jobiMawtus = new Conta(630841470, "ContaCorrente", 28776, "JobiMawtus");
 
-let thomasinLatour = new Conta(4223508636, 'ContaCorrente', 2177, 'ThomasinLatour');
+let thomasinLatour = new Conta(4223508636, "ContaCorrente", 2177, "ThomasinLatour");
 
-let lonnieVerheijden = new Conta(185979521, 'ContaPoupanca', 25994, 'LonnieVerheijden');
+let lonnieVerheijden = new Conta(185979521, "ContaPoupanca", 25994, "LonnieVerheijden");
 
-let alonsoWannan = new Conta(3151956165, 'ContaCorrente', 7601, 'AlonsoWannan');
+let alonsoWannan = new Conta(3151956165, "ContaCorrente", 7601, "AlonsoWannan");
 
-let benditeHuggett = new Conta(2138105881, 'ContaPoupanca', 33196, 'BenditeHuggett');
+let benditeHuggett = new Conta(2138105881, "ContaPoupanca", 33196, "BenditeHuggett");
 
 //lista de clientes
-let listaContas = {
-     1: {cliente: abigaelNatte}, 
-     2: {cliente: ramonConnel}, 
-     3: {cliente: jarretLafuente}, 
-     4: {cliente: anselArdley}, 
-     5: {cliente: jackiShurmer}, 
-     6: {cliente: jobiMawtus}, 
-     7: {cliente: thomasinLatour}, 
-     8: {cliente: lonnieVerheijden}, 
-     9: {cliente: alonsoWannan}, 
-     10: { cliente: benditeHuggett}
-};
-
-// let infoCliente = listaContas.find(item => item.id == 2);
-// console.log(infoCliente.cliente.titular)
-
+let listaContas = [abigaelNatte, ramonConnel, jarretLafuente, anselArdley, jackiShurmer, jobiMawtus, thomasinLatour, lonnieVerheijden, alonsoWannan, benditeHuggett];
 
 //obj banco recebe a lista de clientes
 
-const banco = {
+let banco = {
     clientes: listaContas,
-    consultarClientes(nome){
-        let infoCliente = listaContas[nome];
-        return infoCliente;
-    },
 
-    deposito(nome, valor){
-        
-        console.log(`Depósito realizado, seu novo saldo é: `)
-    },
-
-    //saca se valor atual na conta for maior ou igual ao valor do solicitado
-    saque(valor){
-        if(Conta.saldo >= valor){
-            Conta.saldo -= valor;
-            console.log(`Saque feito com sucesso! Valor atual na conta é de: "R$"${valor}`);
-            return true;
-            
-        } else {
-            console.log("Saldo na conta insuficientes.")
-            return false;
+    consultarCliente(titular) {
+      let resultado = [];
+      for (let i = 0; i < this.clientes.length; i++) {
+        let titularBuscado = this.clientes[i].titular;
+        if (titularBuscado === titular) {
+          return this.clientes[i];
         }
+      }
+      return resultado;
+    },
+
+    deposito(titular, valor){
+        
+        
+    },
+
+    saque(titular, valor){
+
+
+
     }
-}
+};
 
-
-console.log(banco.consultarClientes(10))
+console.log(banco.consultarCliente('LonnieVerheijden'));
